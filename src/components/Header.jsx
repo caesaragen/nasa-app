3276caa1121bb848c9209f85ff826c8468ed4cc1
Joel Aduma <caesaragen@gmail.com>
2022-09-21 00:16:11 +0300

    Create Header, fetch image of the day

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { API_URL } from '../config/config'
+import Moment from 'react-moment';
+
+
+const Header = () => {
+  const [headerImage, setHeaderImage] = useState('')
+  const [date, setDate] = useState('')
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setHeaderImage(data.hdurl)
+        setDate(data.date)
+        console.log(data)
+      })
+  }, [])
+  
+  return (
+      <div>
+          <h1 className="text-center text-3xl">
+        <span className='text-3xl text-[#FFB703]'>Nasa: </span>               
+        <span className='text-3xl text-[#8ECAE6] '>Picture of the day </span><br></br>
+        <span className='text-3xl text-[#8ECAE6] '>
+        <Moment local>{date}</Moment>
+        </span>
+
+        <img src={headerImage} alt="nasa" />
+      </h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+// Configuration for NASA API 
+
+// const API_URL = 'https://api.nasa.gov/planetary/apod?';
+// const API_KEY = 'yoxaHrmDfn2d5dx6E0hDagcy7l2lHAuW6RUunBeQ';
+const API_URL = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+
+export { API_URL };
